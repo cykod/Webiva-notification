b204552597a3ae26c0b6f7a9295e4f81f61710b2
Doug Youch <doug@cykod.com>
2010-10-26 15:10:12 -0400

    Notification module, notify a user or groups of users.

---
+++ app/models/notification_message_user.rb
@@ -0,0 +1,8 @@
+
+class NotificationMessageUser < DomainModel
+  belongs_to :end_user
+  belongs_to :notification_message
+
+  validates_presence_of :end_user_id
+  validates_presence_of :notification_message_id
+end
